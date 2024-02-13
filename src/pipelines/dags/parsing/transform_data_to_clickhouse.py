7ca661e794d4362f41d630dfa4d804fca2e45e15
alexchikov <alex.chikov@outlook.com>
2024-02-13 21:32:08 +0300

    continue with data transforming

--- src/pipelines/dags/parsing/transform_data_to_clickhouse.py
+++ src/pipelines/dags/parsing/transform_data_to_clickhouse.py
@@ -1,6 +1,8 @@
-from parsing.parse import Parser
-from config import DAGConfig as cfg
+from parse import Parser
+from dags.config import DAGConfig as cfg
+from pyspark.sql import SparkSession, functions as F
 import yaml
+import json
 
 with open(f'{cfg.CONFIGS_PATH}/s3.yml') as file:
     config_file = yaml.safe_load(file)
@@ -9,4 +11,43 @@ with open(f'{cfg.CONFIGS_PATH}/s3.yml') as file:
     BUCKET = config_file['bucketname']
     
 def parse_json_data(file: str):
-    ...
+    with open('/home/aleks/Документы/cian_parsing_and_analyse/files/cian_2024-02-12_23:02:20.json') as json_file:
+        json_file_data = json.load(json_file)["data"]["offersSerialized"]
+
+    spark = SparkSession().builder.master('local[1]').appName('Cian parsing').getOrCreate()
+    
+    offerId = json_file_data["id"]
+    isApartments = json_file_data["isApartments"]
+    addedTimestamp = json_file_data["addedTimestamp"]
+    fullUrl = json_file_data["fullUrl"]
+    underground_name = json_file_data["geo"]["undergrounds"][0]["name"]
+    underground_transportType = json_file_data["geo"]["undergrounds"][0]["transportType"]
+    underground_time = json_file_data["geo"]["undergrounds"][0]["time"]
+    flatType = json_file_data["flatType"]
+    description = json_file_data["description"]
+    floorNumber = json_file_data["floorNumber"]
+    price = json_file_data["bargainTerms"]["price"]
+    deposit = json_file_data["bargainTerms"]["deposit"]
+    leaseTermType = json_file_data["bargainTerms"]["leaseTermType"]
+    paymentPeriod = json_file_data["bargainTerms"]["paymentPeriod"]
+    floorsCount = json_file_data["building"]["floorsCount"]
+    buildYear = json_file_data["building"]["buildYear"]
+    photo1 = json_file_data["photos"][0]["fullUrl"]
+    photo2 = json_file_data["photos"][1]["fullUrl"]
+    photo3 = json_file_data["photos"][2]["fullUrl"]
+    phone = json_file_data["phones"]["countryCode"] + json_file_data["phones"]["number"]
+
+    df = spark.createDataFrame({"offerId": offerId,
+                                "isApartments": isApartments,
+                                "addedTimestamp": addedTimestamp,
+                                "fullUrl": fullUrl,
+                                "undergroundName": underground_name,
+                                "undergroundTransportType": underground_transportType,
+                                "undergroundTime": underground_time,
+                                "flatType": flatType,
+                                "description": description,
+                                "floorNumber": floorNumber,
+                                "price": price})
+    df.show()
+
+parse_json_data('cian_2024-02-12_23:02:20.json')
